81a1de7cb5ad0668e23808e77ec3ec538f7c6c20
rohmanahmad <rohmanmail@gmail.com>
2015-10-20 11:24:20 +0700

    add

---
+++ modules/v1/views/index-page.php
@@ -0,0 +1,170 @@
+
+
+    <!-- Header -->
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Rohman Web ID</h1>
+                        <h3>Tempat Berbagi Ilmu Seputar Komputer & Programing</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="https://twitter.com/" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            </li>
+                            <li>
+                                <a href="https://facebook.com" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                            </li>
+                            <li>
+                                <a href="https://plus.google.com" class="btn btn-default btn-lg"><i class="fa fa-google fa-fw"></i> <span class="network-name">Google+</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+
+    <!-- Page Content -->
+
+    <a name="services"></a>
+    <div class="content-section-a">
+
+        <div class="container">
+            <div class="row">
+                <h1>Services</h1>
+                <hr><br/><br/><br/><br/>
+                <div class="col-sm-3">
+                    
+                        <img style="margin-left:24%" src="<?php echo base_url().'assets/img/dog.png';?>" width="50%">
+                    
+                    <hr>
+                        <span>Website Programing</span><br><br>
+                        <p style="font-weight:normal">Kami mempunyai beberapa web developer yang khusus dalam menangani dan pembuatan website.</p>
+                    <hr>
+                    <a class="btn btn-primary">More Info</a>
+                </div>
+                <div class="col-sm-3">
+                    
+                        <img style="margin-left:24%" src="<?php echo base_url().'assets/img/dog.png';?>" width="50%">
+                    
+                    <hr>
+                        <span>Content Management</span><br><br>
+                        <p style="font-weight:normal">Management content dalam website merupakan salah satu keahlian kami untuk melakukannya.</p>
+                    <hr>
+                    <a class="btn btn-primary">More Info</a>
+                </div>
+                <div class="col-sm-3">
+                    
+                        <img style="margin-left:24%" src="<?php echo base_url().'assets/img/dog.png';?>" width="50%">
+                    
+                    <hr>
+                        <span>CEO Analistics</span><br><br>
+                        <p style="font-weight:normal">Meningkatkan ranking website dan menemukan keyword yang cocok untuk website anda</p>
+                    <hr>
+                    <a class="btn btn-primary">More Info</a>
+                </div>
+                <div class="col-sm-3">
+                    
+                        <img style="margin-left:24%" src="<?php echo base_url().'assets/img/dog.png';?>" width="50%">
+                    
+                    <hr>
+                        <span>Training</span><br><br>
+                        <p style="font-weight:normal">Kami membuka kelas training untuk tingkat pemula dan menengah untuk pembangunan website dan networking</p>
+                    <hr>
+                    <a class="btn btn-primary">More Info</a>
+                </div>
+                
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.content-section-a -->
+
+    <a name="contact"></a>
+    <div class="content-section-a">
+
+        <div class="container">
+            <div class="row">
+                <h1>Kontak</h1>
+                <hr>
+                <div class="col-sm-10">
+                    <form class="form-horizontal">
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Nama</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" placeholder="Nama">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">No Telp</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" placeholder="No Telp">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Pesan</label>
+                        <div class="col-sm-10">
+                          <textarea name="pesan" class="form-control" style="height:250px;" placeholder="Masukkan Pesan Disini"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-primary glyphicon glyphicon-send"> Kirim</button>
+                        </div>
+                      </div>
+                    </form>
+                </div>
+                
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.content-section-a -->
+
+	<a  name="contact"></a>
+    <div class="banner">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2>Tetaplah Bersemangat Dalam Menuntut Ilmu :)</h2>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="list-inline banner-social-buttons">
+                        <li>
+                            <a href="https://twitter.com" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        </li>
+                        <li>
+                            <a href="https://facebook.com" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Github</span></a>
+                        </li>
+                        <li>
+                            <a href="https://plus.google.com" class="btn btn-default btn-lg"><i class="fa fa-google fa-fw"></i> <span class="network-name">Google+</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.banner -->
