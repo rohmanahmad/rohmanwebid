95612109f0b18fd7985f98e5f2e5fb302c2f937a
rohmanahmad <rohmanmail@gmail.com>
2015-10-21 12:01:21 +0700

    picasa

--- modules/v2/controllers/Picasa_upload.php
+++ modules/v2/controllers/Picasa_upload.php
@@ -3,8 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Picasa_upload extends MX_Controller {
 
+var $sess_temboo;
+
 	function __construct(){
 		parent::__construct();
+	}
+
+	function set_temboo_session(){
+		if($this->sess_temboo == ''){
+		}
+			$this->sess_temboo=$this->picasa();
 	}
 
 	function index()
@@ -21,5 +29,77 @@ class Picasa_upload extends MX_Controller {
 		$this->load->view('header',$d);
 		$this->load->view('picasa/galery_page');
 		$this->load->view('footer');
+	}
+
+	function get_user($user){
+		$array_users=array(
+				'default'=>array(
+						'username'=>'rohmanwebid@gmail.com',
+						'password'=>'allah is 1'
+					)
+			);
+
+		return $array_users[$user];
+	}
+
+	function upload_photo(){
+		$user=$this->get_user($this->input->post('use_account'));
+		if(!is_array($user))exit('Data tidak tersedia!');
+		$filename=$this->input->post('filename');
+		$file=$_FILES['file']['tmp_name'];
+
+		$albumid='105181859478368215246';
+		$puser=$user['username'];
+		$ppass=$user['password'];
+		
+		$sess=$this->picasa();
+		$this->upload_picasa($sess,$filename,$file,$puser,$ppass,$albumid);
+	}
+	
+	function upload_picasa($session,$filename='',$file='',$puser='',$ppass='',$albumid=''){
+		//$file=$this->upload_photo($file); //matikan jika memanggil function 'tes'
+		// Instantiate the Choreo, using a previously instantiated Temboo_Session object, eg:
+		$postPhoto = new Google_Picasa_PostPhoto($session);
+		// Get an input object for the Choreo
+		$postPhotoInputs = $postPhoto->newInputs();
+		// Set inputs
+		try{
+		$postPhotoInputs->setImageName($filename)//$judul
+			->setClientSecret("rLrd6OE3guBTyx3NwhPenKuG")
+			->setRefreshToken("1/o3G_592X5sxpQ5SO8xvuyIJKxn6IO3S_tZC06HyXjOrBactUREZofsF9C7PrpE-j")
+			->setAlbumID($albumid)//$albumid
+			->setFileContents($file)
+			->setUserID('117107516089877359680')//dunia grup
+			->setClientID("437877361893-v5bgn5lennfolmmogrd0dqpv1nfp1qhs.apps.googleusercontent.com");
+		}catch(Exception $e){
+			$e->getMessage();
+		}
+
+		// Execute Choreo and get results
+		$postPhotoResults = $postPhoto->execute($postPhotoInputs)->getResults();
+		$output=$postPhotoResults->outputArray['Response'];
+
+		//print_r($res);
+		$f = new SimpleXMLElement($output);
+
+		$res=json_decode(str_replace('@','',json_encode($f->content)));
+		$res=str_replace('https','http',$res->attributes->src);
+		return $res;
+	}
+	
+	function picasa(){
+		require(realpath('temboo')."/temboo.php");
+		$session = new Temboo_Session('rohmanwebid', 'myFirstApp', '24b088fea0a14d7eb53e3f13082724c5');
+
+		return $session;
+	}
+
+
+	function form_ex(){
+		$this->load->helper('form');
+		echo form_open_multipart();
+		echo form_upload('file');
+		echo form_submit('submit','upload');
+		echo form_close();
 	}
 }
--- modules/v2/views/picasa/index_page.php
+++ modules/v2/views/picasa/index_page.php
@@ -9,23 +9,23 @@
                     <img style="margin-left:20%;" src="https://lh3.googleusercontent.com/-ryEJ6pI_lzE/ViXdPtz8P8I/AAAAAAAAABU/u6_vhLB-Phg/s177-Ic42/picasa-rohmanweb.png" width="100px" />
                 </div>
                 <div class="col-sm-9">
-					<form>
+					<?=form_open_multipart('v2/picasa_upload/upload_photo')?>
 					  <div class="form-group">
 					    <label for="exampleInputEmail1">Gunakan Akun</label>
-					    <?=form_dropdown('use_account',array('rohman'=>'default'),'','class="form-control"')?>
+					    <?=form_dropdown('use_account',array('default'=>'default'),'','class="form-control"')?>
 					  </div>
 					  <div class="form-group">
 					    <label for="exampleInputPassword1">Simpan Dg Nama</label>
-					    <input name="filename" type="text" class="form-control" id="nama_file" placeholder="">
+					    <input name="filename" type="text" class="form-control" id="nama_file" placeholder="Masukkan Nama">
 					  </div>
 					  <div class="form-group">
 					    <label for="input_file">Gambar</label>
-					    <input name="file" type="file" id="input_file">
+					    <?=form_upload('file_data')?>
 					    <p class="help-block">Pilih File Gambar [ *.jpg,*.png,*.bmp ]</p>
 					  </div>
 					  <button type="submit" class="btn btn-primary">Ambil Link</button>
 					  <a href="<?=site_url('v2/picasa_upload/galery')?>" class="btn btn-default">Lihat Galery</a>
-					</form>
+					<?=form_close()?>
                 </div>
             </div>
 
